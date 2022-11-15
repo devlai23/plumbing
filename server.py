09f225fe64b9c20c762c261d8134b2d740816a64
devlai23 <57195223+devlai23@users.noreply.github.com>
2022-11-15 14:17:14 -0500

    flask

--- server.py
+++ server.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config["MYSQL_HOST"] = "soccerdb.calingaiy4id.us-east-2.rds.amazonaws.com"
+app.config["MYSQL_USER"] = "hog_kim_lai"
+app.config["MYSQL_PASSWORD"] = "7BVjbea4jUrF"
+app.config["MYSQL_DB"] = "capstone_2223_mochinut"
+
+mysql = MySQL(app)
+
 @app.route("/")
 def home():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM Customer')
+    rv = cur.fetchall()
+    customer = str(rv)
+    #cur.execute('INSERT INTO Customer (Customer_ID, First_Name, Last_Name, Birthday, Email) values (9178059231, "Bobby", "Jones", 2005-01-07, "bobbyjones@gmail.com")')
+    #return customer
     return render_template("index.html")
     
 if __name__ == "__main__":
