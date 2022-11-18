7ab704221c0ea7c7ab938c81b5b14f8baca67e5b
devlai23 <57195223+devlai23@users.noreply.github.com>
2022-11-18 14:25:38 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -12,13 +12,35 @@ mysql = MySQL(app)
 
 @app.route("/")
 def home():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM Customer')
-    rv = cur.fetchall()
-    customer = str(rv)
-    #cur.execute('INSERT INTO Customer (Customer_ID, First_Name, Last_Name, Birthday, Email) values (9178059231, "Bobby", "Jones", 2005-01-07, "bobbyjones@gmail.com")')
-    #return customer
+    #cur = mysql.connection.cursor()
+
+
+    # cur.execute('''SELECT * FROM Customer''')
+    # rv = cur.fetchall()
+    # customer = str(rv)
+    
+    #cur.execute('''INSERT INTO Customer VALUES(9178059232, "Jimmy", "Jones", 2000-01-01, "jimmyjones@gmail.com")''')
+    #mysql.connection.commit()
+
+    # cur.close()
     return render_template("index.html")
     
+@app.route("/", methods=['POST'])
+def post():
+    text = request.form
+    cur = mysql.connection.cursor()
+
+    check = "SELECT IF(" + text.get("pnumber") + " in (SELECT Customer_ID from Customer), 1, 0)"
+    print(check)
+    print(text.get("pnumber"))
+    print(cur.execute(check))
+    if cur.execute(check) == 1:
+        return render_template("index.html")
+    str = "INSERT INTO Customer VALUES(" + text.get("pnumber") + ", \'" + text.get("fname") + "\', \'" + text.get("lname") + "\', CAST(\'" + text.get("bday") + "\' as DATE), \'" + text.get("email") + "\')"
+    cur.execute(str)
+    mysql.connection.commit()
+    return render_template("index.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
