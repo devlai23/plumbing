ccd7446a4f929946ebcca8d907b47743b95c722e
natalieekim <60183185+natalieekim@users.noreply.github.com>
2023-02-02 13:38:34 -0500

    php

---
+++ app/htdocs/database-test.php
@@ -0,0 +1,7 @@
+<?php
+    $dbServername = "soccerdb.calingaiy4id.us-east-2.rds.amazonaws.com"; 
+    $dbUsername = "hog_kim_lai";
+    $dbPassword = "7BVjbea4jUrF";
+    $dbName = "capstone_2223_mochinut"; 
+    
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
