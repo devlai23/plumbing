a2a70ca6b7aec761e2362c1f55d1357c05890501
devlai23 <57195223+devlai23@users.noreply.github.com>
2023-05-18 10:09:24 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -374,7 +374,8 @@ def send():
 def analytics():
     #if 'mybutton' in request.form:
     cur = mysql.connection.cursor()
-    query = "SELECT item, COUNT(*) AS popularity FROM Purchases GROUP BY item ORDER BY popularity DESC LIMIT 5"
+    number = int(request.args.get("number"))
+    query = "SELECT item, COUNT(*) AS popularity FROM Purchases GROUP BY item ORDER BY popularity DESC LIMIT " + number
     cur.execute(query)
 
     rows = cur.fetchall()
