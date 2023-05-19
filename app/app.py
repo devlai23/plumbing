301b0806f9e776641274c8832e36027c3950dcf2
devlai23 <57195223+devlai23@users.noreply.github.com>
2023-05-18 20:47:48 -0400

    fixing order?

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ import datetime
 from functools import wraps
 from PIL import Image
 import io
-import Order
+from my_package import Order
 
 ROOT_DIR = os.path.abspath(os.curdir)
 ENV_FILE = find_dotenv()
